db5e520759932de3956bedd3802aaea38a0f5423
Robert C Edwards <139775+robertcedwards@users.noreply.github.com>
2025-07-28 10:19:57 -0500

    Added redirect urls

---
+++ src/redirects.js
@@ -0,0 +1,10 @@
+export default [
+    {
+      path: "/griv",
+      redirect: "https://farcaster.xyz/?launchFrameUrl=https%3A%2F%2Fwww.griv.gg%2F",
+    },
+    {
+      path: "/runner",
+      redirect: "https://farcaster.xyz/?launchFrameUrl=https%3A%2F%2Frunnercoin.lat%2F",
+    },
+  ]
